0e563e6c6861f3bef18b0af12ee059bfcddfda3b
joohw1115 <joohw1115@naver.com>
2019-11-26 14:46:15 +0900

    [Frontend] added url auto complete in request page

--- src/frontend/anim.js
+++ src/frontend/anim.js
@@ -3,3 +3,7 @@ $(document).ready(() => {
         $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
      });
 });
+
+chrome.tabs.getSelected(null, function(tab){
+    document.getElementById("url-input").value = tab.url;
+});
