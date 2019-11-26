56ae4fb804e3bd8dc91f03b20c12dabf0382360b
Kim Woo Kyung <kwk9926@gmail.com>
2019-11-26 22:44:11 +0900

    [Backend] set urls

--- src/backend/picketserver/picketserver/settings.py
+++ src/backend/picketserver/picketserver/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '115.145.179.201', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+	'authenticator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- src/backend/picketserver/picketserver/urls.py
+++ src/backend/picketserver/picketserver/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+#from authenticator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('login/', include('authenticator.urls')),
+#	path('login/', views.control, name='login'),
 ]
