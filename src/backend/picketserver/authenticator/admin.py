02ece2f7d4f17c20cba366c14120e91b463e298f
Kim Woo Kyung <kwk9926@gmail.com>
2019-11-27 19:52:13 +0900

    [Backend] Authenticator admin

--- src/backend/picketserver/authenticator/admin.py
+++ src/backend/picketserver/authenticator/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+#admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ['user_id', 'user_pw', 'user_email']
+
+admin.site.register(User, UserAdmin)
