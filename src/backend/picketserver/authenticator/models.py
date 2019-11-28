f9241f1954bd95d8342bf1e3c3bd7f03d022a54d
jaewan7599 <jaewan7599@skku.edu>
2019-11-28 12:33:24 +0900

    Cart_item table append

--- src/backend/picketserver/authenticator/models.py
+++ src/backend/picketserver/authenticator/models.py
@@ -5,3 +5,11 @@ class User(models.Model):
 	user_id = models.CharField(max_length=1024, primary_key=True)
 	user_pw = models.CharField(max_length=1024)
 	user_email = models.CharField(max_length=1024)
+
+class Cart_items(models.Model):
+	item_id = models.CharField(max_length=1024, primary_key=True)
+	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+	url = models.CharField(max_length=1024)
+	price = models.CharField(max_length=1024)
+	img_url = models.CharField(max_length=1024)
+	item_name = models.CharField(max_length=1024)
