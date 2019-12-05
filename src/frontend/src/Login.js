e88602d86a52472e48ff3c6c7b1e6606527be86c
hyednjs <joohw1115@naver.com>
2019-12-06 01:45:05 +0900

    [Frontend] Login Page server connect complete

--- src/frontend/src/Login.js
+++ src/frontend/src/Login.js
@@ -18,71 +18,78 @@ function HelpMessage(props) {
     </p>
   );
 }
-function failNotification() {
-  NotificationManager.error('Please Check Your ID or Password', 'Login Faild', 2500, () => {
-  });
-}
 
-function createNotification() {
-  NotificationManager.success('Sign in and Enjoy PICKET', 'Success Create Account', 2000);
+function Notification(type,message) {
+  if(type === 'login-fail'){
+    NotificationManager.error(message, 'Login Faild', 2700, () => {});
+  }
+  if(type==='register-fail'){
+    NotificationManager.error(message, 'Register Faild', 2700, () => {});
+  }
+  if(type==='register-success'){
+    NotificationManager.success(message, 'Success Create Account', 2000);
+  }
 }
 
 function Login(props) {
 
-  const [state, setState] = useState({
-    name: "",
-    password: "",
-    email: ""
-  });
+  const [id, setID] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
-  const LoginServer = "http://ec2-13-125-249-233.ap-northeast-2.compute.amazonaws.com:8080/login";
+  const server = "http://ec2-13-125-249-233.ap-northeast-2.compute.amazonaws.com:8080";
 
-  function handleNameChange(e) {
-    setState({ fields: { name: e.target.value } });
-  }
-  function handlePasswordChange(e) {
-    setState({ password: e.target.value });
-  }
   function loginHandler() {
-    console.log(state.name);
-    axios.post(LoginServer, {
-      user_id: state.name,
-      user_pw: state.password
+    axios.post(server +"/login", {
+      user_id: id,
+      user_pw: password
     })
       .then(function (response) {
-        console.log(response);
+        console.log(response.data);
+        if(response.data.status ==='fail') Notification('login-fail',response.data.message);
       })
       .catch(function (error) {
         console.log('err', error);
       });
   }
+
+  function registerHandler() {
+    axios.post(server + "/register", {
+      user_id: id,
+      user_pw: password,
+      user_email :email
+    })
+      .then(function (response) {
+        if(response.data.status === 'fail') Notification('register-fail',response.data.message);
+        else Notification('register-success',response.data.message)
+      })
+      .catch(function (error) {
+        console.log('err', error);
+      });
+  }
+
   return (
     <div class="login-page">
       <img class="logo-img" src="img/logo.png" />
       <div class="form">
+
         <form class="register-form">
           <h2>REGISTER</h2>
-          <input type="text" placeholder="name" />
-          <input type="password" placeholder="password" />
-          <input type="text" placeholder="email address" />
-          <button class="create-btn" type="reset" onClick={createNotification}>create</button>
+          <input type="text" placeholder="name" value={id} onChange={(e) => setID(e.target.value)} />
+          <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="text" placeholder="email address" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <button class="create-btn" type="reset" onClick={registerHandler}>create</button>
           <HelpMessage msg="Already registered? " detail="Sign In" />
         </form>
+
         <form class="login-form">
           <h2>LOGIN</h2>
-          <input
-            type="text"
-            placeholder="username"
-            value={state.name}
-            onChange={handleNameChange} />
-          <input
-            type="password"
-            placeholder="password"
-            value={state.password}
-            onChange={handlePasswordChange} />
+          <input type="text" placeholder="username" value={id} onChange={(e) => setID(e.target.value)} />
+          <input type="password" placeholder="password"  value={password} onChange={(e) => setPassword(e.target.value)}/>
           <button class="login-btn" type="reset" onClick={loginHandler} type="button">login</button>
           <HelpMessage msg="Not registered? " detail="Create an account" />
         </form>
+
         <NotificationContainer />
       </div>
     </div>
