cb448789438231a41f028c9c7ccbb4bf57fb22de
jaewan7599 <jaewan7599@skku.edu>
2019-12-08 21:07:36 +0900

    BERTClass change

--- src/backend/picketserver/reviewanalyzier/model/BERTClass.py
+++ src/backend/picketserver/reviewanalyzier/model/BERTClass.py
@@ -1,3 +1,13 @@
+import numpy as np
+from mxnet.gluon import nn, rnn
+from mxnet import gluon, autograd
+import gluonnlp as nlp
+import mxnet as mx
+
+from kobert.mxnet_kobert import get_mxnet_kobert_model
+from kobert.utils import get_tokenizer
+
+
 class BERTClassifier(nn.Block):
     def __init__(self, bert, num_classes=2, dropout=None, prefix=None, params=None):
         super(BERTClassifier, self).__init__(prefix=prefix, params=params)
